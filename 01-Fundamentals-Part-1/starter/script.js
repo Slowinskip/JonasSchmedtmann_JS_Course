11a79e3b9e247570ed76897d004500dc0636ab96
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-09 11:18:40 +0200

    operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -8,7 +8,6 @@ console.log(firstName);
 
 let jonas_matilda = "JM";
 let $function = 27;
-*/
 
 //Data types primitive
 let number = 22;
@@ -18,3 +17,35 @@ let undefined; //Jeśli zmienna nie posiada przypisanej wartości
 null;
 
 console.log(typeof number);
+*/
+
+//Math operators
+const now = 2037;
+const jonasAge = now - 1991;
+const saraAge = now - 2018;
+
+// console.log(jonasAge, saraAge);
+// console.log(jonasAge * 2, jonasAge / 2);
+
+// const firstName = "Jonas";
+// const lastName = "Smit";
+// console.log(firstName + " " + lastName);
+
+// //Assignment operators
+// let x = 10 + 5;
+// x += 10; // x = x + 10
+// x *= 4; // x = x * 4
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// //Comperison operators
+// console.log(jonasAge > saraAge); // >, <, <=, >=
+// console.log(saraAge >= 23);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (jonasAge + saraAge) / 2;
+console.log(averageAge);
