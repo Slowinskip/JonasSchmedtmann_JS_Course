cb58bada526645561238ab2a796ea3639a1a8336
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-10 22:19:29 +0200

    arrow functions, func call other func,

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -29,7 +29,6 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
-*/
 
 //function declaration
 function calcAge1(brithYear) {
@@ -45,3 +44,44 @@ const calcAge2 = function (brithYear) {
 
 const age2 = calcAge2(1991);
 console.log(age1, age2);
+
+
+//arrow function
+const calcAge3 = (brithYear) => 2037 - brithYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (brithYear, firstName) => {
+  const age = 2037 - brithYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement}`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePices = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePices} pieces of oranges.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 2));
+
+*/
+const calcAge = function (brithYear) {
+  return 2037 - brithYear;
+};
+
+const yearsUntilRetirement = function (brithYear, firstName) {
+  const age = calcAge(brithYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    return retirement;
+  } else return -1;
+  //   return `${firstName} retires in ${retirement}`;
+};
