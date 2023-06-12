596f7246a3f95e0779f118a173a28a19a7627454
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-12 21:38:16 +0200

    dom manipulation, click event

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const maxTemp = [17, 21, 23];
+const maxTemp2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (tempArr) {
+  let string = "";
+
+  for (let i = 0; i < tempArr.length; i++) {
+    const data = ` ${tempArr[i]} in ${i + 1} days ...`;
+    string += data;
+  }
+  return console.log("..." + string);
+};
+
+printForecast(maxTemp);
+printForecast(maxTemp2);
