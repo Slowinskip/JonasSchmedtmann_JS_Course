de76d799f8a860d55ce22daca442c1726df1c444
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-13 18:57:59 +0200

    Project II, class manipulation

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const bntCloseModal = document.querySelector('.close-modal');
+const bntsOpenModal = document.querySelectorAll('.show-modal');
+
+const toggleModal = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < bntsOpenModal.length; i++) {
+  bntsOpenModal[i].addEventListener('click', toggleModal);
+}
+
+bntCloseModal.addEventListener('click', toggleModal);
+overlay.addEventListener('click', toggleModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    toggleModal();
+  }
+});
