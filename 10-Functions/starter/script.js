36cf6f940e65cebf796fb16a7f6e8750d43a9787
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-17 22:14:30 +0200

    Function, default parameters, value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPassangers = 1, price = 199) {
+  //   numPassangers = numPassangers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum: flightNum,
+    numPassangers: numPassangers,
+    price: price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH200', 2, 800);
+*/
+
+const flight = 'LH111';
+const jonas = {
+  name: 'Jonas',
+  passport: 11223344,
+};
+
+const checkIn = function (flightNumb, passanger) {
+  flightNumb = 'LH999';
+  passanger.name = 'Mr ' + passanger.name;
+
+  if (passanger.passport === 11223344) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+
+console.log(flight);
+console.log(jonas);
