ca8ec0446d2e9674cb4074649ff00d1241f2905e
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-06-21 11:31:21 +0200

    fn closure exaples, code challenge II

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -218,7 +218,6 @@ runOnce();
 (function () {
   console.log('This wll never run again');
 })();
-*/
 
 const secureBooking = function () {
   let passengerCount = 0;
@@ -231,3 +230,62 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+f();
+g();
+h();
+f();
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passangers`);
+    console.log(`There ale 3 groups each with ${perGroup} passangers`);
+  }, wait * 1000);
+  console.log(`Will start bboarding in ${wait} seconds`);
+};
+// const perGroup = 1000;
+boardPassengers(100, 3);
+
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, 
+attach an event listener that changes the color of the 
+selected h1 element ('header') to blue, each time the 
+BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) 
+WHY this worked! Take all the time you need. 
+Think about WHEN exactly the callback function is executed, 
+and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+
+*/
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', () => {
+    header.style.color = 'blue';
+    console.log('Works?');
+  });
+})();
